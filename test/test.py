fc8fc259ca99f75f3f3a662f82241e59007897b6
vviveks <vishwavivek0562@outlook.com>
2022-09-07 17:12:20 +0530

    changes

--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 import requests 
 
-resp = requests.post("http://0.0.0.0:5000/predict", files={'file': open('0a6983a5-895e-4e68-9edb-88adf79211e9___RS_Early.B 9072.JPG', 'rb')})
+resp = requests.post("https://cropera.herokuapp.com/predict", files={'file': open('0a6983a5-895e-4e68-9edb-88adf79211e9___RS_Early.B 9072.JPG', 'rb')})
 
 print(resp.text)
